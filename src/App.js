be2ac84566fc6101a5c5f865941cbf1058eec2eb
Keith Crooc <keith.crooc@gmail.com>
2021-04-14 20:21:27 -0400

    adding basic setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hey there keith
+        </p>
+
+          
+      </header>
+    </div>
+  );
+}
+
+export default App;
